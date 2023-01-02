20c756bed7e7654dac77cedb42ca1bee09b14abe
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-01-02 20:35:23 +0200

    removed a specific item

--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,10 @@ const reducer = (state, action) => {
   if (action.type === "CLEAR_CART") {
     return { ...state, cart: [] };
   }
+  if (action.type === "CLEAR_ITEM") {
+    const newCart = state.cart.filter((item) => item.id !== action.payload);
+    return { ...state, cart: newCart };
+  }
 };
 
 export default reducer;
