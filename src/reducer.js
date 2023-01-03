a44ae4ecb90bc16cad4e077b3bcfc3f406933ddd
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-01-03 16:50:51 +0200

    implemented increasing cart

--- src/reducer.js
+++ src/reducer.js
@@ -6,6 +6,15 @@ const reducer = (state, action) => {
     const newCart = state.cart.filter((item) => item.id !== action.payload);
     return { ...state, cart: newCart };
   }
+  if (action.type === "INCREASE") {
+    const tempCart = state.cart.map((item) => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount + 1 };
+      }
+      return item;
+    });
+    return { ...state, cart: tempCart };
+  }
 };
 
 export default reducer;
